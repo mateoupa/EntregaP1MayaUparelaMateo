c9ffa666a72def24706028a5d82f122fd97ae670
Mateo Maya Uparela <mateoupa@gmail.com>
2022-03-21 18:28:21 -0500

    creando modelos

--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class auditor(models.Model):
+    nombre = models.CharField(max_length=20)
+    cargo = models.CharField(max_length=20)
+    email = models.EmailField()
+    
+class registro_producto(models.Model):
+    nombre = models.CharField(max_length=20)
+    precio = models.IntegerField()
+    cantidad =models.IntegerField()
+    
+class almacen(models.Model):
+    nombre = models.CharField(max_length=20)
+    turno = models.IntegerField()
+    
--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def inicio(request):
+    
+    return HttpResponse('<h1> hola <h1>')
